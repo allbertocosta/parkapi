45893e079bfbd3598c2dc4174d659577f8ec1439
Alberto Costa <mihcadevs@gmail.com>
2024-12-30 05:17:22 -0300

    Criação do projeto, Locale e Timezone configurados

---
+++ src/main/java/com/mballem/demo_park_api/config/SpringTimeZoneConfig.java
@@ -0,0 +1,17 @@
+package com.mballem.demo_park_api.config;
+
+import java.util.TimeZone;
+
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.annotation.PostConstruct;
+
+@Configuration
+public class SpringTimeZoneConfig {
+	
+	@PostConstruct
+	public void timezoneConfig() {
+	TimeZone.setDefault(TimeZone.getTimeZone( "America/Sao Paulo"));
+	
+	}
+}
