c53326e4d4793473180aa8927f9eb948c09c9155
Alberto Costa <mihcadevs@gmail.com>
2025-01-12 03:11:09 -0300

    Recurso para altera a senha do usuario

--- src/main/java/com/mballem/demo_park_api/controller/UsuarioController.java
+++ src/main/java/com/mballem/demo_park_api/controller/UsuarioController.java
@@ -2,9 +2,9 @@ package com.mballem.demo_park_api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,5 +36,11 @@ public class UsuarioController {
 		Usuario user = usuarioService.buscarPorId(id);
 		return ResponseEntity.ok(user);
 	}
-
+	
+	@PatchMapping("/{id}")
+	public ResponseEntity<Usuario> updatePassword(@PathVariable Long id, @RequestBody Usuario usuario) {
+		Usuario user = usuarioService.editarSenha(id, usuario.getPassword());
+		return ResponseEntity.ok(user);
+	}
 }
+	
